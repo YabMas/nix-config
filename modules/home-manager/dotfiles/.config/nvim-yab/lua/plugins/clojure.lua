137214e891873c67a14ba4febe720f9ca4293322
Jaap Maaskant <jaapmaaskant@hotmail.com>
2024-12-20 12:05:56 +0100

    another try at parinfer

--- modules/home-manager/dotfiles/.config/nvim-yab/lua/plugins/clojure.lua
+++ modules/home-manager/dotfiles/.config/nvim-yab/lua/plugins/clojure.lua
@@ -10,47 +10,38 @@ return {
       vim.g["conjure#client#clojure#nrepl#connection#auto_repl#enabled"] = false
     end
   },
-  {
-    "julienvincent/nvim-paredit",
-    config = function()
-      local paredit = require("nvim-paredit")
-      paredit.setup(
-        -- {
-        --   keys = {
-        --     ["?"] = { paredit.api.slurp_forwards, "Slurp forwards" },
-        --     ["?"] = { paredit.api.barf_backwards, "Barf backwards" },
-        --     ["?"] = { paredit.api.barf_forwards, "Barf forwards" },
-        --     ["?"] = { paredit.api.slurp_backwards, "Slurp backwards" },
-        --   },
-        -- }
-      )
-    end
-  },
   -- {
-  --   "dundalek/parpar.nvim",
-  --   ft = lisp_dialects,
-  --   dependencies = {
-  --     {
-  --       "gpanders/nvim-parinfer",
-  --       ft = { "clojure" },
-  --       config = function()
-  --         vim.g.parinfer_force_balance = true
-  --       end,
-  --     },
-  --     { "julienvincent/nvim-paredit" } },
+  --   "julienvincent/nvim-paredit",
   --   config = function()
-  --     local paredit = require "nvim-paredit"
-  --     require("parpar").setup {
-  --       paredit = {
-  --         keys = {
-  --           ["<A-H>"] = { paredit.api.slurp_backwards, "Slurp backwards" },
-  --           ["<A-J>"] = { paredit.api.barf_backwards, "Barf backwards" },
-  --           ["<A-K>"] = { paredit.api.barf_forwards, "Barf forwards" },
-  --           ["<A-L>"] = { paredit.api.slurp_forwards, "Slurp forwards" },
-  --         },
-  --       },
-  --     }
-  --   end,
+  --     local paredit = require("nvim-paredit")
+  --     paredit.setup(
+  --       {
+  --         -- keys = {
+  --         --   ["<M-H"] = { paredit.api.slurp_forwards, "Slurp forwards" },
+  --         --   ["<M-J"] = { paredit.api.barf_backwards, "Barf backwards" },
+  --         --   ["<M-K"] = { paredit.api.barf_forwards, "Barf forwards" },
+  --         --   ["<M-L"] = { paredit.api.slurp_backwards, "Slurp backwards" },
+  --         -- },
+  --       }
+  --     )
+  --   end
   -- },
+  {
+    "dundalek/parpar.nvim",
+    ft = lisp_dialects,
+    dependencies = {
+      {
+        "gpanders/nvim-parinfer",
+        ft = { "clojure" },
+        config = function()
+          vim.g.parinfer_force_balance = true
+        end,
+      },
+      { "julienvincent/nvim-paredit" } },
+    config = function()
+      local paredit = require "nvim-paredit"
+      require("parpar").setup { }
+    end,
+  },
   { 'HiPhish/rainbow-delimiters.nvim' }
 }
