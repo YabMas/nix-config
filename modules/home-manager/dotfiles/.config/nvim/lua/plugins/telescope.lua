85e15bfc602488059c11fcbdf6088b03ef7503f4
Yab Mas <jaapmaaskant@hotmail.com>
2025-02-01 11:55:00 +0100

    improved telescope setup

--- modules/home-manager/dotfiles/.config/nvim/lua/plugins/telescope.lua
+++ modules/home-manager/dotfiles/.config/nvim/lua/plugins/telescope.lua
@@ -1,14 +1,25 @@
-local function init_telescope()
-  local builtin = require('telescope.builtin')
-  vim.keymap.set('n', '<leader>ff', builtin.find_files, {noremap = true, desc = "Telescope find files"})
-  vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Telescope live grep" })
-  vim.keymap.set('n', '<leader>lr', builtin.lsp_references, { desc = "Telescope find references" })
-  vim.keymap.set('n', '<leader>ls', builtin.lsp_document_symbols, { desc = "Telescope find document symbols" })
-  vim.keymap.set('n', '<C-f>', builtin.current_buffer_fuzzy_find, { desc = "Telescope find current buffer" })
-end
-
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  init = init_telescope
+  init = function()
+    require('telescope').setup {
+      defaults = {
+        layout_strategy = 'vertical',
+        layout_config = { 
+          height = 0.95, 
+          width = 0.95, 
+          prompt_position = 'bottom',
+          mirror = true
+        },
+      },
+    }
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { noremap = true, desc = "Telescope find files" })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Telescope live grep" })
+    vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = "Telescope search word under cursor or selection" })
+    vim.keymap.set('n', '<leader>lr', builtin.lsp_references, { desc = "Telescope find references" })
+    vim.keymap.set('n', '<leader>lss', builtin.lsp_document_symbols, { desc = "Telescope find document symbols" })
+    vim.keymap.set('n', '<leader>lsa', builtin.lsp_workspace_symbols, { desc = "Telescope find workspace symbols" })
+  end
 }
